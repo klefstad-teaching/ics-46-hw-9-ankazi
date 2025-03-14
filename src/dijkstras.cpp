5c81caabfe95e68dacda4982e6b3f332ea6bceb9
ankazi <ankazi@uci.edu>
2025-03-14 21:51:38 +0000

    More output formatting

--- src/dijkstras.cpp
+++ src/dijkstras.cpp
@@ -57,7 +57,8 @@ void print_path(const vector<int>& path, int total) {
     }
     for (size_t i = 0; i < path.size(); i++) {
         cout << path[i];
-        if (i != path.size() - 1) cout << " ";
+		cout << " ";
+        //if (i != path.size() - 1) cout << " ";
     }
-    cout << "\nTotal cost: " << total << endl;
+    cout << "\nTotal cost is " << total << endl;
 }
