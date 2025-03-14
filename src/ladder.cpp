e84319ef6284fa890acb1b676afb992d854407dc
ankazi <ankazi@uci.edu>
2025-03-14 21:46:14 +0000

    Fixed print formatting

--- src/ladder.cpp
+++ src/ladder.cpp
@@ -89,7 +89,7 @@ void print_word_ladder(const vector<string>& ladder) {
     cout << "Word Ladder: ";
     for (size_t i = 0; i < ladder.size(); ++i) {
         cout << ladder[i];
-        if (i < ladder.size() - 1) cout << " -> ";
+        if (i < ladder.size() - 1) cout << " ";
     }
     cout << endl;
 }
